31e48b573466058fea7869430ba9998f5c60ca43
booydar <ayd98@mail.ru>
2022-02-07 19:04:47 +0300

    training starts

---
+++ x_transformers/NN/x_transformers/__init__.py
@@ -0,0 +1,2 @@
+from .x_transformers import XTransformer, Encoder, Decoder, CrossAttender, Attention, TransformerWrapper, ViTransformerWrapper, ContinuousTransformerWrapper
+from .autoregressive_wrapper import AutoregressiveWrapper
